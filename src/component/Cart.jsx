5868f8569abd81ceb143928492211c081d400c72
_GrandMaster_ MRF <fachruddin18genz@gmail.com>
2022-02-07 19:02:34 +0700

    update

--- src/component/Cart.jsx
+++ src/component/Cart.jsx
@@ -90,7 +90,7 @@ function Cart(props) {
                 </div>
             </div>
 
-            {/* Moda; Pay */}
+            {/* Modal Pay */}
             <Transition.Root show={openPay} as={Fragment}>
                 <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" initialFocus={cancelButtonRef} onClose={setOpenPay}>
                     <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
